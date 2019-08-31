afc7d65fb6790aade298f9633d253c0e075c8cde
DonAnk <11donank22@gmail.com>
2019-08-31 18:27:39 +0530

    added models

--- search/models.py
+++ search/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    FEMALE = 'F'
+    MALE = 'M'
+    OTHERS = 'O'
+    SEX_CHOICE = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHERS, 'Others'),
+    ]
+    age = models.PositiveSmallIntegerField(),
+    sex = models.CharField(
+        max_length=1,
+        choices=SEX_CHOICE,
+        default=MALE,
+    )
+
+class Query(models.Model):
+    query = models.CharField(max=250),
+    location = models.CharField(max=250)
